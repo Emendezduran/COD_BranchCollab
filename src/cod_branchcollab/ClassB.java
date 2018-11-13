b2c8caa604a2c7050824194822e8bff4789ea484
emendezduran <emendezduran@nautilus25.danielcastelao.org>
2018-11-13 09:23:05 +0100

    commit

--- src/cod_branchcollab/ClassB.java
+++ src/cod_branchcollab/ClassB.java
@@ -9,13 +9,6 @@ package cod_branchcollab;
  *
  * @author emendezduran
  */
-public class ClassA {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    //RAMA MASTER
+public class ClassB {
+    
 }
